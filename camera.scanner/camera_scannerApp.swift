367e5f46ad8da95d88d8c941e02976ad4966de72
Anthony <anthonioez@gmail.com>
2022-09-15 15:05:42 +0100

    Initial Commit

---
+++ camera.scanner/camera_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  camera_scannerApp.swift
+//  camera.scanner
+//
+//  Created by Anthony Ezeh on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct camera_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
